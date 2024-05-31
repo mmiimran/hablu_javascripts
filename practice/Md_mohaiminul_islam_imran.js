d8544840d2f621fe70e392e998df7ca0acf6f662
mmiimran <go2imran6@gmail.com>
2024-05-31 11:56:54 +0600

    practice

---
+++ practice/Md_mohaiminul_islam_imran.js
@@ -0,0 +1,105 @@
+//task 1
+let carName = "Volvo";
+
+//task 2
+
+let firstName = "John", lastName = "Doe", age = 35;
+
+//task 3
+
+x = 10;
+y = 5;
+x *= y; // x will be 50
+
+//task 4
+
+let length = 16; // data type: Number
+let lastName1 = "Johnson"; // data type: String
+
+const x = {
+  firstName: "John",  
+  lastName1: "Doe"
+}; // data type: Object
+
+
+//task 5
+
+function myFunction() {
+    alert("Hello World!");
+  }
+  
+  // Call the function and see the output
+  myFunction();
+
+  //task 6
+
+  // Create the object
+const person = {
+    name: "John",
+    age: 50
+  };
+  
+  // Access the object properties and show the alert
+  alert(person.name + " is " + person.age);
+
+
+
+// task 7
+
+// Function to be called when the button is clicked
+function handleClick() {
+    alert('md mohaiminul islam imran');
+  }
+
+  // Add event listener to the button
+document.getElementById('myButton').onclick = handleClick;
+  
+
+//task 8
+
+// Alert the number of items in an array
+const cars = ["Volvo", "Jeep", "Mercedes"];
+alert("Number of items in the cars array: " + cars.length);
+
+// Change the first item of Brand to "Ford"
+const Brand = ["Volvo", "Jeep", "Mercedes"];
+Brand[0] = "Ford";
+alert("First item of Brand array is now: " + Brand[0]);
+
+ 
+// task 9
+
+//part1 random number
+const randomNumber = Math.random();
+alert("Random number: " + randomNumber);
+
+//part2 
+//Return the largest number of 10 and 20
+const largestNumber = Math.max(10, 20);
+
+//part3 sqroot of 9
+const squareRoot = Math.sqrt(9);
+alert("Square root of 9: " + squareRoot);
+
+
+//task 10
+
+const x1 = 10;
+const y = 5;
+alert(x > y); // This will alert true
+
+//part2
+const age1 = 16;
+alert(age < 18 ? "Too young" : "Old enough");
+
+
+
+
+
+
+
+
+
+
+
+
